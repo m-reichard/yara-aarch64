6fcfbd82de0c4ffcea2b7df0fc956ff884f74f07
Maggie Reichard <mreichard@protonmail.com>
2026-01-02 15:05:19 -0500

    updating rsize_t type

--- src/internals/stream.rs
+++ src/internals/stream.rs
@@ -1,116 +1,11 @@
-// use std::io::{Read, Result, Write};
-// use yara_sys::YR_STREAM;
-
-// #[cfg(all(windows, target_arch = "aarch64"))]
-// pub type YaraSizeT = usize;
-
-// #[cfg(not(any(all(windows, target_arch = "aarch64"))))]
-// pub type YaraSizeT = usize; //yara_sys::size_t;
-
-// pub struct ReadStream<'r> {
-//     reader: &'r mut dyn Read,
-//     result: Result<()>,
-// }
-
-// impl<'r> ReadStream<'r> {
-//     pub fn new(reader: &'r mut dyn Read) -> Self {
-//         Self {
-//             reader,
-//             result: Ok(()),
-//         }
-//     }
-
-//     pub fn as_yara(&mut self) -> YR_STREAM {
-//         YR_STREAM {
-//             user_data: self as *mut ReadStream as *mut std::os::raw::c_void,
-//             read: Some(stream_read_func),
-//             write: None,
-//         }
-//     }
-
-//     pub fn result(self) -> Result<()> {
-//         self.result
-//     }
-// }
-
-// pub struct WriteStream<'w> {
-//     writer: &'w mut dyn Write,
-//     result: Result<()>,
-// }
-
-// impl<'w> WriteStream<'w> {
-//     pub fn new(writer: &'w mut dyn Write) -> Self {
-//         Self {
-//             writer,
-//             result: Ok(()),
-//         }
-//     }
-
-//     pub fn as_yara(&mut self) -> YR_STREAM {
-//         YR_STREAM {
-//             user_data: self as *mut Self as *mut std::os::raw::c_void,
-//             read: None,
-//             write: Some(stream_write_func),
-//         }
-//     }
-
-//     pub fn result(self) -> Result<()> {
-//         self.result
-//     }
-// }
-
-// unsafe extern "C" fn stream_read_func(
-//     ptr: *mut ::std::os::raw::c_void,
-//     size: YaraSizeT,
-//     count: YaraSizeT,
-//     user_data: *mut ::std::os::raw::c_void,
-// ) -> YaraSizeT {
-//     let this: &mut ReadStream = &mut *(user_data as *mut ReadStream);
-//     if this.result.is_err() {
-//         return 0;
-//     }
-
-//     let buffer = std::slice::from_raw_parts_mut(ptr as *mut u8, (size * count) as usize);
-//     let result = this.reader.read(buffer);
-
-//     match result {
-//         // FIXME: what if read_size is not a multiple of size ?
-//         Ok(read_size) => read_size as YaraSizeT / size,
-//         Err(e) => {
-//             this.result = Err(e);
-//             0
-//         }
-//     }
-// }
-
-// unsafe extern "C" fn stream_write_func(
-//     ptr: *const ::std::os::raw::c_void,
-//     size: YaraSizeT,
-//     count: YaraSizeT,
-//     user_data: *mut ::std::os::raw::c_void,
-// ) -> YaraSizeT {
-//     let this: &mut WriteStream = &mut *(user_data as *mut WriteStream);
-//     if this.result.is_err() {
-//         return 0;
-//     }
-
-//     let buffer = std::slice::from_raw_parts(ptr as *const u8, (size * count) as usize);
-//     let result = this.writer.write_all(buffer);
-
-//     match result {
-//         Ok(()) => count,
-//         Err(e) => {
-//             this.result = Err(e);
-//             0
-//         }
-//     }
-// }
-
 use std::io::{Read, Result, Write};
 use yara_sys::YR_STREAM;
 
-// Use u32 consistently since that's what the bindings expect
-pub type YaraSizeT = u32;
+#[cfg(all(windows, target_arch = "aarch64"))]
+pub type YaraSizeT = usize;
+
+#[cfg(not(any(all(windows, target_arch = "aarch64"))))]
+pub type YaraSizeT = yara_sys::rsize_t;
 
 pub struct ReadStream<'r> {
     reader: &'r mut dyn Read,
@@ -179,7 +74,8 @@ unsafe extern "C" fn stream_read_func(
     let result = this.reader.read(buffer);
 
     match result {
-        Ok(read_size) => (read_size as YaraSizeT) / size,
+        // FIXME: what if read_size is not a multiple of size ?
+        Ok(read_size) => read_size as YaraSizeT / size,
         Err(e) => {
             this.result = Err(e);
             0
