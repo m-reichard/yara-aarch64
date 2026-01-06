9c46fd159d7b8f564d60f65303de55136c53420b
Maggie Reichard <mreichard@protonmail.com>
2026-01-06 16:43:44 -0500

    removing Copy trait

--- yara-sys/bindings/yara-4.5.5-aarch64-unknown-linux-musl.rs
+++ yara-sys/bindings/yara-4.5.5-aarch64-unknown-linux-musl.rs
@@ -32,7 +32,6 @@ impl<T> ::std::clone::Clone for __IncompleteArrayField<T> {
         *self
     }
 }
-impl<T> ::std::marker::Copy for __IncompleteArrayField<T> {}
 impl<T> ::std::fmt::Debug for __IncompleteArrayField<T> {
     fn fmt(&self, fmt: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
         fmt.write_str("__IncompleteArrayField")
@@ -7769,7 +7768,7 @@ fn bindgen_test_layout_RE_AST() {
     );
 }
 #[repr(C, packed)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 pub struct RE {
     pub flags: u32,
     pub code: __IncompleteArrayField<u8>,
