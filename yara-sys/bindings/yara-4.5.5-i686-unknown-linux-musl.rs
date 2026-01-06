0f9377b302af5b711d23977182a8e15e5198fa71
Maggie Reichard <mreichard@protonmail.com>
2026-01-06 14:02:17 -0500

    revising Linux x86 bindings file, removing line causing overflow error

--- yara-sys/bindings/yara-4.5.5-i686-unknown-linux-musl.rs
+++ yara-sys/bindings/yara-4.5.5-i686-unknown-linux-musl.rs
@@ -3115,15 +3115,6 @@ pub struct max_align_t {
     pub __clang_max_align_nonce1: ::std::os::raw::c_longlong,
     pub __clang_max_align_nonce2: f64,
 }
-#[allow(clippy::unnecessary_operation, clippy::identity_op)]
-const _: () = {
-    ["Size of max_align_t"][::std::mem::size_of::<max_align_t>() - 24usize];
-    ["Alignment of max_align_t"][::std::mem::align_of::<max_align_t>() - 8usize];
-    ["Offset of field: max_align_t::__clang_max_align_nonce1"]
-        [::std::mem::offset_of!(max_align_t, __clang_max_align_nonce1) - 0usize];
-    ["Offset of field: max_align_t::__clang_max_align_nonce2"]
-        [::std::mem::offset_of!(max_align_t, __clang_max_align_nonce2) - 8usize];
-};
 pub type int_least8_t = __int_least8_t;
 pub type int_least16_t = __int_least16_t;
 pub type int_least32_t = __int_least32_t;
